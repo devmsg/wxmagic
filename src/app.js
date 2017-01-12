311fede5683f4b0a42e64b4e6e0f28190601745e
likaiqi <likaiqi@jubaopen365.com>
2017-01-13 00:18:39 +0800

    修复 异步问题

--- src/app.js
+++ src/app.js
@@ -3,30 +3,27 @@ import util from './utils/util'
 
 export default class {
 
-  globalData = {
-    userInfo: null
-  };
+	globalData = {
+		userInfo: null
+	};
 
-  async onLaunch() {
-    //调用API从本地缓存中获取数据
+	async onLaunch() {
+		let res  = await wx.getStorage({ key: "logs" });
+		let logs = res.data || [];
+		logs.unshift(Date.now());
+		await wx.setStorage({
+			key : 'logs',
+			data: logs
+		});
+	}
 
-    let res  = await wx.getStorage({ key: "logs" });
-    let logs = res.data || [];
-
-    logs.unshift(Date.now());
-    await wx.setStorage({
-      key : 'logs',
-      data: logs
-    });
-  }
-
-  async getUserInfo() {
-    if (this.globalData.userInfo) {
-      return this.globalData.userInfo;
-    }
-    await wx.login();
-    let res                  = await wx.getUserInfo();
-    this.globalData.userInfo = res.userInfo;
-    return res.userInfo;
-  }
+	async getUserInfo() {
+		if (this.globalData.userInfo) {
+			return this.globalData.userInfo;
+		}
+		await wx.login();
+		let res                  = await wx.getUserInfo();
+		this.globalData.userInfo = res.userInfo;
+		return res.userInfo;
+	}
 }
